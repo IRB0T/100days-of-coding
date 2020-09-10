fed8c207a6011347dd97431791511cadcadc532e
raj upadhyay <upadhyayraj007@gmail.com>
2020-09-10 11:32:46 +0530

    Added Bash Folder and Day 1 Scripts

---
+++ Bash/Day-1/comments_and_variables.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#this is comment
+echo "Hello World" #comment
+
+#system variable
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+#custom varibale
+
+name=bob
+
+echo My name is $name
