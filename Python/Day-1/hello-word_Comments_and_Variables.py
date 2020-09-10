da06a2666be03dc849ab523e628218ba5d47f11c
raj upadhyay <upadhyayraj007@gmail.com>
2020-09-10 15:19:35 +0530

    Python Day-1 script Added

---
+++ Python/Day-1/hello-word_Comments_and_Variables.py
@@ -0,0 +1,21 @@
+
+print("HELLO WORLD")
+
+#single line comment
+
+'''
+this
+is
+multi
+line
+comments
+'''
+
+#keywords, for,in,else,if,elif etc
+
+#varibles
+
+name = "raj"
+
+print(name)
+print("my name is ",name)
