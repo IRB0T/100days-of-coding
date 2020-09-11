202384eddb165671a1fad120636908c4807097d6
raj upadhyay <upadhyayraj007@gmail.com>
2020-09-11 11:16:24 +0530

    Day-2 How to read input from user

---
+++ Python/Day-2/Read Input.py
@@ -0,0 +1,14 @@
+#let's take input from user
+
+print("Enter Name")
+name = input()
+print("Your Name is ",name)
+
+#let's print enter name in input()
+name = input("Enter Your Name : ")
+print("Your name is ",name)
+
+#let's take multiple input
+name = [i for i in input().split()]
+print(name)
+
