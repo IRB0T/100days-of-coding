8718abda67a8bad478cd889c4209d894c93717b5
raj upadhyay <upadhyayraj007@gmail.com>
2020-09-13 12:31:14 +0530

    Day-4 Conditional Statement

---
+++ Python/Day-4/Conditional_statement.py
@@ -0,0 +1,31 @@
+#let's print command line arguments
+
+number = 10
+
+if number < 10:
+    print("Less then 10")
+elif number == 10:
+    print("Equal To 10")
+else:
+    print("Grater then 10")
+
+'''
+simple if:
+if condition:
+    statement
+
+simple if else
+if condition:
+    statement
+else:
+    statement
+
+simple if elif else
+
+if condtition:
+    stmt
+elif condition:
+    stmt
+else
+    stmt
+'''
