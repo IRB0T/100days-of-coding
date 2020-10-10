b2c9a7c45ef49356594ea7d3947764121fec6724
raj upadhyay <upadhyayraj007@gmail.com>
2020-10-10 11:13:32 +0530

    Day-13 For loop

---
+++ Python/Day-13/For Loop.py
@@ -0,0 +1,22 @@
+#let's open file and write data in it.
+'''
+for condition:
+	stmt
+	stmt
+	stmt
+	stmt
+'''
+
+num1 = 10
+for i in range(0,num1):
+	print(i)
+print("*"*20)
+for i in range(num1,0,-1):
+	print(i)
+print("*"*20)
+for i in range(0,num1,2):
+	print(i)
+print("*"*20)
+for i in range(num1,0,-2):
+	print(i)
+
